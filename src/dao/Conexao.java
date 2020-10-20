79b6c67200417d89e52962d03b5f6cd04c09f5ee
Gabriel Augusto <gabrielaugusto753@gmail.com>
2020-10-19 23:55:45 -0300

    criando métodos sql da tabela associação

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -12,6 +12,16 @@ public class Conexao {
 	
 	private Connection connection;
 	
+	
+	
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
 	public Conexao() {
 		try {
 			Class.forName(driver);
