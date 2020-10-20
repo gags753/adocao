c0114fb7e1a41617c0123b3a6fbb9e9957e8c45f
Gabriel Augusto <gabrielaugusto753@gmail.com>
2020-10-19 21:49:27 -0300

    criando conexão com o banco de dados

---
+++ src/dao/Conexao.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+	
+	private final static String driver = "org.postgresql.Driver";
+	private final static String URL = "jdbc:postgresql://localhost:5432/adocao";
+	private final static String USUARIO = "postgres";
+	private final static String SENHA = "402789";
+	
+	private Connection connection;
+	
+	public Conexao() {
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(URL, USUARIO, SENHA);
+			System.out.println("Conexão bem sucedida");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Conexao();
+	}
+	
+}
